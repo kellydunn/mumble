4bee2f9a01ff5db19b2eb1a32a630a79f2e5c803
Kelly <defaultstring@gmail.com>
2013-01-14 23:27:21 -0800

    [src] I give up for tonight :P

--- src/mumble.c
+++ src/mumble.c
@@ -13,9 +13,11 @@ static void button_handler(const monome_event_t *e, void *user_data) {
   if(event_type == MONOME_BUTTON_DOWN) {
     // TODO play midi note
     //      Midi note on == 0x90
+    printf("Button down\n");
   } else {
     // TODO stop midi note
     //      Midi not off == 0x80
+    printf("Button up\n");
   }
 }
 
@@ -24,5 +26,15 @@ int main() {
   // connect to a monome
   // register callbacks
   // 
+  monome_t *monome;
+  char *buf;
+  asprintf(&buf, "osc.udp://127.0.0.1:%s/%s", "19431", "/mumble");
+
+  monome = monome_open(buf, "8000");
+  monome_register_handler(monome, MONOME_BUTTON_DOWN, button_handler, monome);
+  monome_register_handler(monome, MONOME_BUTTON_UP, button_handler, monome);
+
+  for(;;) {};
+
   return 0;
 }
